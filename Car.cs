71046f457796fbd7ab9e8a97301ae3ee6e7db58d
UesliLaskajs <uesli.laska@gmail.com>
2024-09-17 19:33:48 +0200

    OOP-Learning-Purposes

---
+++ Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassApp
+{
+    internal class Car
+    {
+
+        private string _carName;
+        private string _carBrand;
+
+        public string CarName { get => _carName; set => _carName = value; }
+        public string CarBrand { get => _carBrand; set => _carBrand = value; }
+
+        public Car(string carname,string carBrand) {
+            CarBrand = carBrand;
+            CarName = carname;
+            
+
+            Console.WriteLine($"This is a {carname} of the {carBrand} Brand ");
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Car audi=new Car("Q7","Audi");
+            Car benz = new Car("Cls", "Mercedez-Benz");
+            
+            Console.ReadKey();
+        }
+    }
+}
